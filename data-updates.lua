2ece5116330e8272ec5c0230293d40b5d5ec13da
Andrew <andrew.weinmann@gmail.com>
2021-01-03 10:28:54 -0600

    Balance tweak Handle oil not present Lower Cargo Ships dependency

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,20 @@
-data.raw.resource["crude-oil"].infinite = false
+if data.raw.resource["crude-oil"] then
+    data.raw.resource["crude-oil"].infinite = false
+
+    data.raw.resource["crude-oil"].minable =
+        {
+            mining_time = 1,
+            results = {
+                {
+                    type = "fluid",
+                    name = "crude-oil",
+                    amount_min = 20,
+                    amount_max = 20,
+                    probability = 1
+                }
+            }
+        }
+end
 
 if data.raw.resource["deep_oil"] then
     data.raw.resource["deep_oil"].infinite = false
