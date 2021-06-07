de1b7a1ca52040eeb270fed81094932d5c5d7ad0
Andrew <andrew.weinmann@gmail.com>
2021-06-06 19:22:16 -0500

    Version 1.1.2

--- data-updates.lua
+++ data-updates.lua
@@ -1,15 +1,20 @@
 if data.raw.resource["crude-oil"] then
-    data.raw.resource["crude-oil"].infinite = false
 
+    data.raw["mining-drill"]["pumpjack"].mining_speed = 2
+
+    data.raw.resource["crude-oil"].infinite = false
+    data.raw.resource["crude-oil"].minimum = 25000
+    data.raw.resource["crude-oil"].normal = 100000
+    data.raw.resource["crude-oil"].infinite_depletion_amount = 5
     data.raw.resource["crude-oil"].minable =
         {
-            mining_time = 1,
+            mining_time = 0.275,
             results = {
                 {
                     type = "fluid",
                     name = "crude-oil",
-                    amount_min = 20,
-                    amount_max = 20,
+                    amount_min = 5,
+                    amount_max = 5,
                     probability = 1
                 }
             }
